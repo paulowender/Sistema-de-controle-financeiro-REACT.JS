e445918e0913a530a993e4bc5cb3160248726a41
Paulo Wender <opaulowender@gmail.com>
2024-05-28 01:12:01 -0400

    update theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,12 +3,30 @@ import { createTheme } from "@mui/material";
 const themeDark = createTheme({
     palette: {
         mode: "dark",
+        primary: {
+            main: "#008B61",
+        },
+        secondary: {
+            main: "#19857b",
+        },
+        error: {
+            main: "#f44336",
+        },
     },
 });
 
 const themeLight = createTheme({
     palette: {
         mode: "light",
+        primary: {
+            main: "#008B61",
+        },
+        secondary: {
+            main: "#19857b",
+        },
+        error: {
+            main: "#f44336",
+        },
     },
 });
 
