766790e7f96d715d6bcf4c0e54af34a58befd315
Paulo Wender <opaulowender@gmail.com>
2024-05-30 16:22:11 -0400

    app title form lang

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { tr } from './lang';
 import App from './pages/index';
 
 const container = document.getElementById('root');
+const title = document.getElementById('title');
+
+title.innerHTML = tr('appName');
 const root = createRoot(container);
 
 root.render(
